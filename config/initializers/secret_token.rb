b3a865f685214135f74c8a72da18e66b71dc1d0e
Marcello Avagliano <info@railszilla.com>
2012-06-01 11:08:53 +0200

    initial project push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecurityApp::Application.config.secret_token = '5aab3c8873c2407d3d75c12e0f5434a30f40ee79963239e16d2c6c74000cf9b71585ef1c8bcfbe06fb11feca2f9b60553291ce1a578c154f5821d9a78b6ecc2c'
